e5603940e39b0e54e020d5af6e6d5a8e3a54c993
Mahsa Shariati <mahsa.shariati1995m@gmail.com>
2025-08-19 15:43:44 +0200

    best

---
+++ test1.py
@@ -0,0 +1,19 @@
+print("hello word")
+print("add 10 ")
+
+# List of 10 best countries (example)
+best_countries = [
+    "Switzerland",
+    "Canada",
+    "Japan",
+    "Germany",
+    "Australia",
+    "Sweden",
+    "Norway",
+    "Netherlands",
+    "New Zealand",
+    "Denmark"
+]
+
+print("10 Best Countries:")
+for country in best_countries:
